2a6b130d4f2d611a54662024d28857dfe527ec07
Umar Khan <khanumar0108@gmail.com>
2024-05-16 20:26:20 +0530

    add contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,29 @@
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="py-4 space-y-6">
+      <div className="flex flex-col gap-1">
+        <span className="text-sm text-gray-400">Information</span>
+        <a className="text-2xl" href="mailto:info@blocklionscapital.com">
+          info@blocklionscapital.com
+        </a>
+      </div>
+      <div className="flex flex-col gap-1">
+        <span className="text-sm text-gray-400">Press Inquiries</span>
+        <a className="text-2xl" href="mailto:press@blocklionscapital.com">
+          press@blocklionscapital.com
+        </a>
+      </div>
+      <div className="flex flex-col gap-1">
+        <span className="text-sm text-gray-400">Investor Inquiries</span>
+        <a
+          className="text-2xl"
+          href="mailto:inv_inquiries@blocklionscapital.com"
+        >
+          inv_inquiries@blocklionscapital.com
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
