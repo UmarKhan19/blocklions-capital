54806e746b591e913715a0b3fdaf1b0b89a26c5f
Umar Khan <khanumar0108@gmail.com>
2024-05-16 17:43:46 +0530

    add Navbar

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-
-export default function Footer() {
+const Footer = () => {
   return <div>Footer</div>;
-}
+};
+
+export default Footer;
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,5 +1,60 @@
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const navLinks = [
+  {
+    label: "About",
+    link: "/",
+  },
+  {
+    label: "Portfolio",
+    link: "/portfolio",
+  },
+  {
+    label: "Contact",
+    link: "/contact",
+  },
+  {
+    label: "Open Source",
+    link: "/open-source",
+  },
+];
+
 const Navbar = () => {
-  return <nav className=""></nav>;
+  return (
+    <nav className="space-y-4">
+      <>
+        <img src="/images/logo.png" className="w-48" />
+      </>
+      <>
+        <ul className="text-[1.75vh] flex text-gray-700 border-y justify-between items-center px-4 py-2 border-gray-500 divide-x divide-gray-500 gap-4 text-center">
+          {navLinks.map(({ label, link }, i) => {
+            return (
+              <li key={i} className="w-full">
+                <NavItem label={label} link={link} />
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    </nav>
+  );
+};
+
+function NavItem({ label, link }) {
+  return (
+    <NavLink
+      to={link}
+      className={({ isActive }) => (isActive ? "font-medium text-black" : "")}
+    >
+      {label}
+    </NavLink>
+  );
+}
+
+NavItem.propTypes = {
+  label: PropTypes.string,
+  link: PropTypes.string,
 };
 
 export default Navbar;
